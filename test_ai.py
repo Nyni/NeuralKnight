c31a037149508ac4437b0e7dcc6c25f95a2bb8ac
HoangNguyen55 <HoangNKNguyen@proton.me>
2024-04-20 16:34:38 -0400

    add script to test ai

---
+++ test_ai.py
@@ -0,0 +1,22 @@
+import chess
+import torch
+import preprocess
+from random import randint
+from AI import *
+
+MIN_ELO = 2000
+MAX_TRAINING_GAMES = 10
+games = preprocess.parse_pgn('libchess_db.pgn', MIN_ELO, MAX_TRAINING_GAMES)
+
+model = ChessCNN()
+model.load_state_dict(torch.load('chess_model.pt'))
+model.eval()
+
+with torch.no_grad():
+    game = games[randint(0, len(games) - 1)]
+    game_state = game
+    for _ in range(randint(1, (len([i for i in game.mainline_moves()]) -1))):
+        game_state = game_state.next() # type: ignore
+    print(make_move(model, game_state.board(), chess.WHITE)) # type: ignore
+
+
